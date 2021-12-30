bd7faec8897b03f94faa61c67747802ef408f2f7
Zalart <me@zalart.com>
2021-12-30 21:18:28 +0300

    Basic test for app component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Input from "./Input";
 
 function App() {
   return (
-    <div className="container">
+    <div className="container" data-test="component-app">
         <h1>Jotto</h1>
      <Congrats success={true} />
         <Input secretWord="party" />
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import {shallow} from 'enzyme'
+import {findByTestAttr} from "../test/testUtils";
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
+const setup = () => {
+  return shallow(<App />);
+}
+test('renders without error', () => {
+const wrapper = setup();
+const appComponent = findByTestAttr(wrapper, 'component-app');
+expect(appComponent).toHaveLength(1);
 });
